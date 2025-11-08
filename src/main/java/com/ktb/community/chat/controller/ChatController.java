72e4f8777cee1f748abd0519bbc3b471b256d51f
sh123456-boop <hash04@naver.com>
2025-11-08 13:45:24 +0900

    chat controller 응답 통일화

--- src/main/java/com/ktb/community/chat/controller/ChatController.java
+++ src/main/java/com/ktb/community/chat/controller/ChatController.java
@@ -5,6 +5,7 @@ import com.ktb.community.chat.dto.ChatRoomListResDto;
 import com.ktb.community.chat.dto.MyChatListResDto;
 import com.ktb.community.chat.service.ChatService;
 import com.ktb.community.chat.service.ChatServiceImpl;
+import com.ktb.community.dto.ApiResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,63 +24,63 @@ public class ChatController {
 
     //    그룹채팅방 개설
     @PostMapping("/room/group/create")
-    public ResponseEntity<?> createGroupRoom(@RequestParam String roomName,
+    public ApiResponseDto<Object> createGroupRoom(@RequestParam String roomName,
                                              @AuthenticationPrincipal(expression = "userId") Long userId){
         chatService.createGroupRoom(roomName,userId);
-        return ResponseEntity.ok().build();
+        return ApiResponseDto.success();
     }
 
     //    그룹채팅목록조회
     @GetMapping("/room/group/list")
-    public ResponseEntity<?> getGroupChatRooms(){
+    public ApiResponseDto<Object> getGroupChatRooms(){
         List<ChatRoomListResDto> chatRooms = chatService.getGroupchatRooms();
-        return new ResponseEntity<>(chatRooms, HttpStatus.OK);
+        return ApiResponseDto.success(chatRooms);
     }
 
     //    그룹채팅방참여
     @PostMapping("/room/group/{roomId}/join")
-    public ResponseEntity<?> joinGroupChatRoom(@PathVariable Long roomId,
+    public ApiResponseDto<Object> joinGroupChatRoom(@PathVariable Long roomId,
                                                @AuthenticationPrincipal(expression = "userId") Long userId){
         chatService.addParticipantToGroupChat(roomId, userId);
-        return ResponseEntity.ok().build();
+        return ApiResponseDto.success();
     }
 
     //    이전 메시지 조회
     @GetMapping("/history/{roomId}")
-    public ResponseEntity<?> getChatHistory(@PathVariable Long roomId,
+    public ApiResponseDto<Object> getChatHistory(@PathVariable Long roomId,
                                             @AuthenticationPrincipal(expression = "userId") Long userId){
         List<ChatMessageDto> chatMessageDtos = chatService.getChatHistory(roomId, userId);
-        return new ResponseEntity<>(chatMessageDtos, HttpStatus.OK);
+        return ApiResponseDto.success(chatMessageDtos);
     }
 
     //    채팅메시지 읽음처리
     @PostMapping("/room/{roomId}/read")
-    public ResponseEntity<?> messageRead(@PathVariable Long roomId,
+    public ApiResponseDto<Object> messageRead(@PathVariable Long roomId,
                                          @AuthenticationPrincipal(expression = "userId") Long userId){
         chatService.messageRead(roomId, userId);
-        return ResponseEntity.ok().build();
+        return ApiResponseDto.success();
     }
 
     //    내채팅방목록조회 : roomId, roomName, 그룹채팅여부, 메시지읽음개수
     @GetMapping("/my/rooms")
-    public ResponseEntity<?> getMyChatRooms(@AuthenticationPrincipal(expression = "userId") Long userId){
+    public ApiResponseDto<Object> getMyChatRooms(@AuthenticationPrincipal(expression = "userId") Long userId){
         List<MyChatListResDto> myChatListResDtos = chatService.getMyChatRooms(userId);
-        return new ResponseEntity<>(myChatListResDtos, HttpStatus.OK);
+        return ApiResponseDto.success(myChatListResDtos);
     }
 
     //    채팅방 나가기
     @DeleteMapping("/room/group/{roomId}/leave")
-    public ResponseEntity<?> leaveGroupChatRoom(@PathVariable Long roomId,
+    public ApiResponseDto<Object> leaveGroupChatRoom(@PathVariable Long roomId,
                                                 @AuthenticationPrincipal(expression = "userId") Long userId){
         chatService.leaveGroupChatRoom(roomId, userId);
-        return ResponseEntity.ok().build();
+        return ApiResponseDto.success();
     }
 
     //    개인 채팅방 개설 또는 기존roomId return
     @PostMapping("/room/private/create")
-    public ResponseEntity<?> getOrCreatePrivateRoom(@RequestParam Long otherMemberId,
+    public ApiResponseDto<Object> getOrCreatePrivateRoom(@RequestParam Long otherMemberId,
                                                     @AuthenticationPrincipal(expression = "userId") Long userId){
         Long roomId = chatService.getOrCreatePrivateRoom(otherMemberId, userId);
-        return new ResponseEntity<>(roomId, HttpStatus.OK);
+        return ApiResponseDto.success(roomId);
     }
 }
