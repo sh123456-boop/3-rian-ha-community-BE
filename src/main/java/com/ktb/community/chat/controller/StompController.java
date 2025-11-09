a8c3a0d179a4550d8ee6f3c879c34cc0112a98a1
sh123456-boop <hash04@naver.com>
2025-11-10 08:47:49 +0900

    채팅 기능 구현 완료

--- src/main/java/com/ktb/community/chat/controller/StompController.java
+++ src/main/java/com/ktb/community/chat/controller/StompController.java
@@ -3,6 +3,7 @@ package com.ktb.community.chat.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ktb.community.chat.dto.ChatMessageDto;
 import com.ktb.community.chat.dto.ChatMessageReqDto;
+import com.ktb.community.chat.mapper.DtoMapper;
 import com.ktb.community.chat.service.ChatServiceImpl;
 import com.ktb.community.chat.service.RedisPubSubService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -17,18 +18,23 @@ public class StompController {
 
     private final SimpMessageSendingOperations messageTemplate;
     private final ChatServiceImpl chatServiceImpl;
+    private final DtoMapper dtoMapper;
     private final RedisPubSubService pubSubService;
 
-    public StompController(SimpMessageSendingOperations messageTemplate, ChatServiceImpl chatServiceImpl, RedisPubSubService pubSubService) {
+    public StompController(SimpMessageSendingOperations messageTemplate, ChatServiceImpl chatServiceImpl, DtoMapper dtoMapper, RedisPubSubService pubSubService) {
         this.messageTemplate = messageTemplate;
         this.chatServiceImpl = chatServiceImpl;
+        this.dtoMapper = dtoMapper;
         this.pubSubService = pubSubService;
     }
 
     @MessageMapping("/{roomId}")
-    public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatmessageDto) throws JsonProcessingException {
+    public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) throws JsonProcessingException {
 //        /publish/roomId 형태로 오면 해당 room에 메세지 저장
-        chatServiceImpl.saveMessage(roomId, chatmessageDto);
-        chatmessageDto.setRoomId(roomId);
+        chatServiceImpl.saveMessage(roomId, chatMessageReqDto);
+        chatMessageReqDto.setRoomId(roomId);
+        ChatMessageDto chatMessageDto = dtoMapper.toDto(chatMessageReqDto);
+        messageTemplate.convertAndSend("/v1/topic/"+roomId, chatMessageDto);
+
     }
 }
---
+++ src/main/java/com/ktb/community/chat/mapper/DtoMapper.java
@@ -0,0 +1,34 @@
+package com.ktb.community.chat.mapper;
+
+import com.ktb.community.chat.dto.ChatMessageDto;
+import com.ktb.community.chat.dto.ChatMessageReqDto;
+import com.ktb.community.chat.repository.ChatMessageRepository;
+import com.ktb.community.entity.User;
+import com.ktb.community.exception.BusinessException;
+import com.ktb.community.exception.ErrorCode;
+import com.ktb.community.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class DtoMapper {
+    private final UserRepository userRepository;
+
+    public DtoMapper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ChatMessageDto toDto(ChatMessageReqDto chatMessageReqDto) {
+        User user = userRepository.findById(chatMessageReqDto.getSenderId()).orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+
+        ChatMessageDto chatMessageDto = ChatMessageDto.builder()
+                .message(chatMessageReqDto.getMessage())
+                .roomId(chatMessageReqDto.getRoomId())
+                .senderId(chatMessageReqDto.getSenderId())
+                .createdAt(Instant.now())
+                .nickName(user.getNickname())
+                .build();
+        return chatMessageDto;
+    }
+}
