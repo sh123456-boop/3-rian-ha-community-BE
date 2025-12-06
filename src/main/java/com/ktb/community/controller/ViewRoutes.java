cb3c13b8532f1a0c9996faf5383801f09745d428
sh123456-boop <hash04@naver.com>
2025-12-06 16:43:17 +0900

    privacy 경로 수정

--- src/main/java/com/ktb/community/controller/ViewRoutes.java
+++ src/main/java/com/ktb/community/controller/ViewRoutes.java
@@ -10,7 +10,7 @@ public class ViewRoutes implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/v1/terms").setViewName("legal/terms");
-        registry.addViewController("/v1/privacy").setViewName("/legal/privacy");
+        registry.addViewController("/v1/privacy").setViewName("legal/privacy");
 
     }
 }
